11bbf4a785e88c88504b07c01a3b314af1acb96d
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2024-04-15 23:33:52 +0530

    Refactor Courses page component to use useEffect hook and update code formatting

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,50 +1,53 @@
-import React from 'react'
-import CourseCard from '../components/CourseCard'
-import Container from '../components/Container'
+import React, { useEffect } from "react";
+import CourseCard from "../components/CourseCard";
+import Container from "../components/Container";
 
 function Courses() {
-    const data = [
-        { id: 1, title: 'React', description: 'Learn React', price: 1500, src: 'https://images.pexels.com/photos/5905709/pexels-photo-5905709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-        { id: 2, title: 'Nodejs', description: 'Learn Node', price: 1200, src: 'https://images.pexels.com/photos/5905709/pexels-photo-5905709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-        { id: 3, title: 'Express', description: 'Learn Express', price: 1100, src: 'https://images.pexels.com/photos/5905709/pexels-photo-5905709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-        { id: 3, title: 'Express', description: 'Learn Express', price: 1100, src: 'https://images.pexels.com/photos/5905709/pexels-photo-5905709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-        { id: 3, title: 'Express', description: 'Learn Express', price: 1100, src: 'https://images.pexels.com/photos/5905709/pexels-photo-5905709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-        { id: 3, title: 'Express', description: 'Learn Express', price: 1100, src: 'https://images.pexels.com/photos/5905709/pexels-photo-5905709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+	const [data, setData] = React.useState([]);
 
-    ]
-
-    useEffect(() => {
-		if (!user) return navigate("/authUser");
-
-		fetch("/api/user/my-courses")
+	useEffect(() => {
+		fetch("/api/user/all-courses")
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data);
 				setData(data.data);
 			});
-	}, [navigate, user]);
-    
-    return (
-        <Container>
-            <div className='flex justify-center pb-10'>
-                <div className=' w-[100%] md:w-[50%]  flex justify-between overflow-hidden rounded-xl bg-white'>
-                    <input className='p-2 rounded-full flex-1 outline-none' type='Search' placeholder='Search Course Category...' />
-                    <button className='px-2'>Search</button>
-                </div>
-            </div>
-            <div className='grid grid-cols-1 md:grid-cols-4  gap-5'>
-                {
-                    data.map((course) => {
-                        return (
-                            <div className='mx-auto flex justify-items-start'>
-                                <CourseCard delay={0} btnText={"Go to Course"} key={course.id} title={course.title} src={course.src} description={course.description} price={course.price} />
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </Container>
-    )
+	}, []);
+
+	return (
+		<Container>
+			<div className="flex justify-center pb-10">
+				<div className=" w-[100%] md:w-[50%]  flex justify-between overflow-hidden rounded-xl bg-white">
+					<input
+						className="p-2 rounded-full flex-1 outline-none"
+						type="Search"
+						placeholder="Search Course Category..."
+					/>
+					<button className="px-2">Search</button>
+				</div>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-4  gap-5">
+				{data.map((course, index) => {
+					return (
+						<div
+							className="mx-auto flex justify-items-start"
+							key={index}
+						>
+							<CourseCard
+								delay={0}
+								btnText={"Go to Course"}
+								key={course.id}
+								title={course.title}
+								src={course.src}
+								description={course.description}
+								price={course.price}
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</Container>
+	);
 }
 
-export default Courses
+export default Courses;
