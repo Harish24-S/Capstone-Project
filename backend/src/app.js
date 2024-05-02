ff9d8f6d2118cc7d0acceda706d39b74b3b995f6
Harish Sarika <79039916+Harish24-S@users.noreply.github.com>
2024-05-02 14:12:45 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,15 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import morgan from "morgan"
+import path from "path"
 
 const app = express()
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
     credentials : true
 }))
+
+const  __dirname = path.resolve();
 
 app.use(express.json({limit : "20kb"}))
 app.use(express.urlencoded({extended:true,limit : "20kb"}))
@@ -23,5 +26,10 @@ import uploaderRouter from "./routes/uploaderRoutes/uploader.routes.js"
 app.use("/api/user",userRouter)
 app.use("/api/uploader",uploaderRouter)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+  })
 
 export { app }
